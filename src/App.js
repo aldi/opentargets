e560d08e2098f78939ffa4919e6c5dfd26e595c8
aldi <aldiduzha@gmail.com>
2021-01-09 21:45:38 +0200

    Update App & Add Error Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './Components/Error/ErrorFallback';
+import DataTable from './Components/DataTable/DataTable';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Feel free to edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <section className="section">
+      <div className="column is-8-widescreen mx-auto">
+        <p className="title is-3 has-text-weight-bold p-2">Genes associated with lung carcinoma</p>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <DataTable />
+        </ErrorBoundary>
+      </div>
+    </section>
   );
 }
 
